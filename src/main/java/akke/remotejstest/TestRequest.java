fba84cb589fdc3647606ee626a1c84fb973e253a
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-04 23:28:24 +0300

    Scripted auto-commit on change (2019-11-04 23:28:24) by gitwatch.sh

--- src/main/java/akke/remotejstest/TestRequest.java
+++ src/main/java/akke/remotejstest/TestRequest.java
@@ -7,17 +7,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TestRequest extends AbstractActor {
-    private MultiValuedMap<String,String> store = new HashMap<>();
-    @Override
-    public Receive createReceive(){
-        return ReceiveBuilder.create()
-                .match(StoreTestResault.class, m -> {
-                store.put(m.getID(), m.getValue());
-                System.out.println("receive message! "+m.toString());
-                })
-                .match(GetMessage.class, req -> sender().tell(
-                        new StoreTestResault(req.getKey(), store.get(req.getKey())), self())
-                ).build();
-    }
-}
+//public class TestRequest extends AbstractActor {
+//    private MultiValuedMap<String,String> store = new HashMap<>();
+//    @Override
+//    public Receive createReceive(){
+//        return ReceiveBuilder.create()
+//                .match(StoreTestResault.class, m -> {
+//                store.put(m.getID(), m.getValue());
+//                System.out.println("receive message! "+m.toString());
+//                })
+//                .match(GetMessage.class, req -> sender().tell(
+//                        new StoreTestResault(req.getKey(), store.get(req.getKey())), self())
+//                ).build();
+//    }
+//}
