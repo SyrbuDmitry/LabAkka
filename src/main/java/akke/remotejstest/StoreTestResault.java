66f882f763976bef47bf492fd04d4e167dc89ecd
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-04 23:42:16 +0300

    Scripted auto-commit on change (2019-11-04 23:42:15) by gitwatch.sh

--- src/main/java/akke/remotejstest/StoreTestResault.java
+++ src/main/java/akke/remotejstest/StoreTestResault.java
@@ -3,7 +3,19 @@ package akke.remotejstest;
 import com.sun.xml.internal.xsom.impl.scd.Iterators;
 
 import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.Map;
 
-public class StoreTestResault extends Map {
+public class StoreTestResault {
+    private String ID, value;
+    StoreTestResault(String ID,String value){
+        this.ID=ID;
+        this.value=value;
+    }
+    public String getID() {
+        return ID;
+    }
+    public String getValue() {
+        return value;
+    }
 }
