a8ab66af83d35c19847e1426815745daa7e5cd7e
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-04 23:05:23 +0300

    Scripted auto-commit on change (2019-11-04 23:05:23) by gitwatch.sh

--- src/main/java/akke/remotejstest/StroreActor.java
+++ src/main/java/akke/remotejstest/StroreActor.java
@@ -1,4 +1,22 @@
 package akke.remotejstest;
 
-public class StroreActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StroreActor extends AbstractActor {
+    private Map<String,String> store = new HashMap<>();
+    @Override
+    public Receive createReceive(){
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+                    store.put(m.getKey(), m.getValue());
+                    System.out.println("receive message" + m.toString());
+                })
+                .match(GetMessage.class, req -> sender().tell(
+                        new StoreMessage(req.getKey(), store.get(req.getKey())), self())
+                ).build();
+    }
 }
