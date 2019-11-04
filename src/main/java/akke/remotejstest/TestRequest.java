c216c096b482ebcba85a1054535e9bfcdef48607
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-04 23:38:13 +0300

    Scripted auto-commit on change (2019-11-04 23:38:13) by gitwatch.sh

--- src/main/java/akke/remotejstest/TestRequest.java
+++ src/main/java/akke/remotejstest/TestRequest.java
@@ -2,13 +2,14 @@ package akke.remotejstest;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
-
+import org.apache.commons.collections4.map.MultiValueMap;
+import
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class TestRequest extends AbstractActor {
-    private MultiValuedMap<String,String> store = new MultiValuedMap<>();
+    private MultiValueMap<String,String> store = new MultiValueMap<>();
     @Override
     public Receive createReceive(){
         return ReceiveBuilder.create()
