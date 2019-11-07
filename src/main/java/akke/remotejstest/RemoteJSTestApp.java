a354649e178ee4e81a0cbcab663e4bc3d0aa7ce8
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-07 18:59:22 +0300

    Scripted auto-commit on change (2019-11-07 18:59:22) by gitwatch.sh

--- src/main/java/akke/remotejstest/RemoteJSTestApp.java
+++ src/main/java/akke/remotejstest/RemoteJSTestApp.java
@@ -1,10 +1,12 @@
 package akke.remotejstest;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 
 public class RemoteJSTestApp {
     public static void main(String[] args){
         ActorSystem system = ActorSystem.create("lab4");
-
+        ActorRef storeActor = system.actorOf(Props.create(TestRequest.class));
     }
 }
