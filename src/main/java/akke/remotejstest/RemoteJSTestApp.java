1f5db6d19ca6376ef17b92cc1ee7165320c36fd5
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-07 19:04:56 +0300

    Scripted auto-commit on change (2019-11-07 19:04:56) by gitwatch.sh

--- src/main/java/akke/remotejstest/RemoteJSTestApp.java
+++ src/main/java/akke/remotejstest/RemoteJSTestApp.java
@@ -3,10 +3,14 @@ package akke.remotejstest;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.routing.RoundRobinRoutingLogic;
 
 public class RemoteJSTestApp {
     public static void main(String[] args){
         ActorSystem system = ActorSystem.create("lab4");
-        ActorRef storeActor = system.actorOf(Props.create(TestRequest.class));
+        ActorRef router = system.actorOf(
+                new Props(TestRequest.class).withRouter(new RoundRobinRoutingLogic(5));
+        )
+        //ActorRef storeActor = system.actorOf(Props.create(TestRequest.class));
     }
 }
