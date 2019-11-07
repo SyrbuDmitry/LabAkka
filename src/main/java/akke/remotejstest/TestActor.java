df8b8f411ebe8991fea58fddee715b2b6834b311
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-07 20:52:43 +0300

    Scripted auto-commit on change (2019-11-07 20:52:43) by gitwatch.sh

--- src/main/java/akke/remotejstest/TestActor.java
+++ src/main/java/akke/remotejstest/TestActor.java
@@ -3,13 +3,20 @@ package akke.remotejstest;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class TestActor extends AbstractActor {
     @Override
     public Receive createReceive(){
         return ReceiveBuilder.create()
                 .match(TestScript.class, m -> {
-                    store.put(m.getID(), m.getResult());
-                    System.out.println("receive message! "+m.toString());
+                    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+                    engine.eval(jscript);
+                    Invocable invocable = (Invocable) engine;
+                    return invocable.invokeFunction(functionName, params).toString();
+
                 })
                 .build();
     }
