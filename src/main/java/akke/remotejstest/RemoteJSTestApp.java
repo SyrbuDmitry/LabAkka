f8bc45f0b4a3a5b0b29b165787f11e6614bc9893
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-08 23:53:28 +0300

    Scripted auto-commit on change (2019-11-08 23:53:27) by gitwatch.sh

--- src/main/java/akke/remotejstest/RemoteJSTestApp.java
+++ src/main/java/akke/remotejstest/RemoteJSTestApp.java
@@ -9,6 +9,7 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.routing.RoundRobinPool;
+import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
