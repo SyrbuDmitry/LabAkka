436e52a48009aa4fd46a98ddec05015e10ad5946
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-08 23:35:09 +0300

    Scripted auto-commit on change (2019-11-08 23:35:09) by gitwatch.sh

--- src/main/java/akke/remotejstest/RemoteJSTestApp.java
+++ src/main/java/akke/remotejstest/RemoteJSTestApp.java
@@ -18,6 +18,6 @@ public class RemoteJSTestApp {
         ActorRef RouteActor = system.actorOf(Props.create(RouterActor.class),"mainRouter");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        MainHttp instance = new MainHttp(system);
+        MainHttp
     }
 }
