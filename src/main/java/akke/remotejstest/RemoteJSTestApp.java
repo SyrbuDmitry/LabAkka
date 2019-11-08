d850ef800f2c4920a997e81c7736bfb2ccee602c
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-08 23:34:09 +0300

    Scripted auto-commit on change (2019-11-08 23:34:09) by gitwatch.sh

--- src/main/java/akke/remotejstest/RemoteJSTestApp.java
+++ src/main/java/akke/remotejstest/RemoteJSTestApp.java
@@ -3,7 +3,9 @@ package akke.remotejstest;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
 import akka.routing.RoundRobinPool;
+import akka.stream.ActorMaterializer;
 
 public class RemoteJSTestApp {
     public static void main(String[] args){
@@ -14,6 +16,8 @@ public class RemoteJSTestApp {
 //                "Router for lab"
 //        );
         ActorRef RouteActor = system.actorOf(Props.create(RouterActor.class),"mainRouter");
-        
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(system);
     }
 }
