1f27759e9dbf6632e5d266bbb94051d0bda50310
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-10 15:15:40 +0300

    Scripted auto-commit on change (2019-11-10 15:15:40) by gitwatch.sh

--- src/main/java/akke/remotejstest/PostRequestBody.java
+++ src/main/java/akke/remotejstest/PostRequestBody.java
@@ -1,4 +1,8 @@
 package akke.remotejstest;
 
 public class PostRequestBody {
-}
+    public String packageId;
+    public String JsScript;
+    public String functionName;
+    public Test[] tests;
+ }
--- src/main/java/akke/remotejstest/Test.java
+++ src/main/java/akke/remotejstest/Test.java
@@ -1,4 +1,7 @@
 package akke.remotejstest;
 
 public class Test {
+    public String testName;
+    public String expectedResult;
+    public String params;
 }
