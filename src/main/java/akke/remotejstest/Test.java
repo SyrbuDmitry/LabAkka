a363f560a32d5efac493df840b84bd6b0f4282c3
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-10 19:40:44 +0300

    Scripted auto-commit on change (2019-11-10 19:40:44) by gitwatch.sh

--- src/main/java/akke/remotejstest/Test.java
+++ src/main/java/akke/remotejstest/Test.java
@@ -2,9 +2,17 @@ package akke.remotejstest;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public class Test {
     public String testName;
     public String expectedResult;
     public String params;
-    Test(){}
+    Test(@JsonProperty("testName") String testName,
+         @JsonProperty("expectedResult") String expectedResult,
+         @JsonProperty("params") String params){
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
 }
