8cc9229448ea20ff5539c823c54609b70758aea2
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-10 17:33:17 +0300

    Scripted auto-commit on change (2019-11-10 17:33:17) by gitwatch.sh

--- src/main/java/akke/remotejstest/TestMarshal.java
+++ src/main/java/akke/remotejstest/TestMarshal.java
@@ -1,6 +1,14 @@
 package akke.remotejstest;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class TestMarshal {
     String packageId;
     String jsScript;
+    @JsonCreator
+    public TestMarshal(@JsonProperty("packageID") String id, @JsonProperty("jsScript") String name) {
+        this.packageId = id;
+        this.jsScript = name;
+    }
 }
