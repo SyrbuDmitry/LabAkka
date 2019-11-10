efde7356c7a6697ab997b1bb68149afcea7e57e4
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-10 19:29:38 +0300

    Scripted auto-commit on change (2019-11-10 19:29:38) by gitwatch.sh

--- src/main/java/akke/remotejstest/PostRequestBody.java
+++ src/main/java/akke/remotejstest/PostRequestBody.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.xml.bind.annotation.XmlElement;
 import java.lang.reflect.Array;
 import java.util.List;
 
@@ -15,7 +14,7 @@ public class PostRequestBody {
     public String JsScript;
 
     public String functionName;
-    @XmlElement(name = "tests")
+
     public List<Test> tests;
     PostRequestBody(){
     }
