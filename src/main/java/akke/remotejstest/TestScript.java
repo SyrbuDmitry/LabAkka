c11c8d2e4a4ccd4428848a56d15717e29538453c
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-10 19:56:35 +0300

    Scripted auto-commit on change (2019-11-10 19:56:35) by gitwatch.sh

--- src/main/java/akke/remotejstest/TestScript.java
+++ src/main/java/akke/remotejstest/TestScript.java
@@ -2,7 +2,7 @@ package akke.remotejstest;
 
 public class TestScript {
     public String functionBody, functionName;
-    public int[] params
+    public int[] params;
     public int packageID;
     TestScript(int packageID,String functionName, String functionBody, int[] params){
         this.packageID = packageID;
