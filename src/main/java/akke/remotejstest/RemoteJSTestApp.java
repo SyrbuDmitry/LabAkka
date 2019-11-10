fef43aa680ae0b58d3110ac72addf473a5919122
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-10 16:29:44 +0300

    Scripted auto-commit on change (2019-11-10 16:29:44) by gitwatch.sh

--- src/main/java/akke/remotejstest/RemoteJSTestApp.java
+++ src/main/java/akke/remotejstest/RemoteJSTestApp.java
@@ -55,7 +55,9 @@ public class RemoteJSTestApp extends AllDirectives {
         return
                 route(
                         pathSingleSlash(() ->
-                            get( () -> )
+                            get( () ->
+                                    
+                            )
                         ),
                         pathSingleSlash(() ->
                                 post(() -> entity(Jackson.unmarshaller(PostRequestBody.class),msg -> {
